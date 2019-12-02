2371868c46a3575f2c40bcf237142244dde0ca72
Philip Enchin <Philip@Ench.in>
2019-12-01 17:09:51 -0800

    Set up project with server and client apps

---
+++ client/components/ColourPreview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ColourPreview(props) {
+  return (
+    <div className="ColourPreview">
+      ColourPreview
+    </div>
+  );
+}
+
+export default ColourPreview;
---
+++ client/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ColourPreview from './components/ColourPreview.jsx';
+
+ReactDOM.render(<ColourPreview />, document.getElementById('app'));
