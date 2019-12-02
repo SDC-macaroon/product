b770020fbb55a691edc5d4c0af10691b2ccddbf9
Philip Enchin <Philip@Ench.in>
2019-12-02 14:49:55 -0500

    Reimplement preliminary seed script using bulkWrite

--- database/seed.js
+++ database/seed.js
@@ -2,24 +2,21 @@ const mongoose = require('mongoose');
 const {Colour, Product} = require('./database.js');
 const db = require('./index.js');
 
-// for (let productId = 2001; productId < 2100; productId++) {
-
-// }
-
+const products = [...Array(100)]
+  .map((_, i) => i + 2001)
+  .map(productId => ({
+    productId, colours: {
+      colourName: 'colour ' + productId,
+      colour: productId,
+      logoUrl: 'logo url ' + productId,
+      frontUrl: 'front url ' + productId,
+      backUrl: 'back url ' + productId,
+    }
+  }))
+  .map(document => ({insertOne: {document}}))
 
 mongoose.connection.createCollection('products')
   .then(() => mongoose.connection.dropCollection('products'))
-  .then(() => Product.create({
-    productId: 2001,
-    colours: [
-      {
-        colourName: 'blue',
-        colour: 5,
-        logoUrl: 'logo url',
-        frontUrl: 'front url',
-        backUrl: 'back url',
-      }
-    ]
-  }))
+  .then(() => Product.bulkWrite(products))
   .then(() => mongoose.connection.close())
   .catch(err => console.log(err.message));
