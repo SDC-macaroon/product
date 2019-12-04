244fb26258da0f471eb3284c57a3cdab5427f6e4
Philip Enchin <Philip@Ench.in>
2019-12-03 22:02:26 -0500

    Add some extra bits to .eslintrc to kill some unncecessary linting complaints

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: 'airbnb'
+  extends: 'airbnb',
+  rules: {
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+  },
+  env: {
+    browser: true,
+    node: true,
+  }
 };
