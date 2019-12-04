57db097255ab56b9101f8e46edc50d9da12b8d7e
Philip Enchin <Philip@Ench.in>
2019-12-03 22:12:44 -0500

    Refine unsplash.example.js

--- APIs/unsplash.example.js
+++ APIs/unsplash.example.js
@@ -1,8 +1,6 @@
 const Unsplash = require('unsplash-js').default;
-if (global.fetch === undefined) {
-  global.fetch = require('node-fetch');
-}
+global.fetch = require('node-fetch');
 
-const  accessKey = 'UNSPLASH ACCESS KEY GOES HERE';
+const accessKey = 'UNSPLASH ACCESS KEY GOES HERE';
 
 module.exports = new Unsplash({accessKey});
