0cc84b01b92cf43f2995bef89c427ca4216f76bc
Philip Enchin <Philip@Ench.in>
2019-12-05 11:32:10 -0500

    Add route in server without any meaningful functionality

--- server/index.js
+++ server/index.js
@@ -5,4 +5,8 @@ const port = 1729;
 
 app.use(express.static('dist/'));
 
+app.get('/api/productPreview/:productId/colours', (req, res) => {
+  res.send(`colour list:  ${req.params.productId}`);
+});
+
 app.listen(port, () => console.log(`listening on port ${port}`));
