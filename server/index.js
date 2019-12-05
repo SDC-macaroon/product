6b7b63a487b8c2899397c157817ddc804d48c2df
Philip Enchin <Philip@Ench.in>
2019-12-04 20:38:53 -0500

    Add route as placeholder

--- server/index.js
+++ server/index.js
@@ -5,4 +5,8 @@ const port = 3000;
 
 app.use(express.static('dist/'));
 
+app.get('/api/productPreview/:productId/:colourName', (req, res) => {
+  res.end();
+});
+
 app.listen(port, () => console.log(`listening on port ${port}`));
