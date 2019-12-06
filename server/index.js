57206090ae66e460654f5e9e9f24227423e47cc4
Philip Enchin <Philip@Ench.in>
2019-12-05 20:46:04 -0500

    Route implemented (but without existing database function)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { imageUrlsForColour } = require('../database/database');
+const { imageUrlsForColour, productData } = require('../database/database');
 
 const app = express();
 const port = 1729;
@@ -10,6 +10,11 @@ app.get('/api/productPreview/:productId/:colourName', (req, res) => {
   imageUrlsForColour(req.params.productId, req.params.colourName)
     .then(results => res.json(results))
     .catch(err => res.json(err));
+});
+
+app.get('/api/productPreview/:productId', (req, res) => {
+  productData(req.params.productId)
+    .then(results => res.json(results));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
