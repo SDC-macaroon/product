a258b48efe23f84ff71ef00fdb6a2109795c1cb5
Philip Enchin <Philip@Ench.in>
2019-12-08 17:02:42 -0500

    Build out ProductList component to show list product links at "/" route

--- client/productList/components/ProductList.jsx
+++ client/productList/components/ProductList.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ProductList = () => (<div>ProductList</div>);
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('api/productPreview/all')
+      .then(result => result.json())
+      .then(allProducts => setProducts(allProducts));
+  }, []);
+
+  return (
+    <div className="ProductList">
+      {products.map(({ productId, productName }) => (
+        <div className="productLink" key={productId}>
+          <a href={`product/${productName.toLowerCase().replace(/ /g, '-')}/${productId}`}>
+            {productName}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ProductList;
