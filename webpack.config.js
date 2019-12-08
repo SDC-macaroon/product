209e878230d4bf5bd6b60e98eb256823e00f63e6
Philip Enchin <Philip@Ench.in>
2019-12-07 22:06:08 -0500

    move client/* -> client/productPage/*

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/index.jsx',
+  entry: './client/productPage/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
