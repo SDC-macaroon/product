6d01c2c3be54638a7a404a0ebecb90fc1dcd9a91
Philip Enchin <Philip@Ench.in>
2019-12-09 12:52:21 -0800

    Add fetch call to api for product information and display

--- client/productPage/components/ColourPreview.jsx
+++ client/productPage/components/ColourPreview.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-function ColourPreview() {
+function ColourPreview({ productId }) {
+  const [productData, setProductData] = useState({});
+
+  useEffect(() => {
+    fetch(`/api/productPreview/${productId}`)
+      .then(result => result.json())
+      .then(data => setProductData(data));
+  }, []);
+
   return (
     <div className="ColourPreview">
-      ColourPreview
+      <ul>
+        <li>{productData.productName}</li>
+        <li>{productData.productId}</li>
+        <ul>{(productData.colours || []).map(colour => <li>{colour.colourName}</li>)}</ul>
+      </ul>
     </div>
   );
 }
+
+ColourPreview.propTypes = {
+  productId: PropTypes.string.isRequired,
+};
 
 export default ColourPreview;
